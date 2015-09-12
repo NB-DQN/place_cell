3b200310dc1b380c7e3b98cbc74915cbdf609869
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-09-13 00:11:57 +0900

    internal virtual coordinate

--- place_cell/deterministic_place_cell.py
+++ place_cell/deterministic_place_cell.py
@@ -3,16 +3,25 @@ from place_cell import PlaceCell
 class DeterministicPlaceCell(PlaceCell):
     def __init__(self, environment):
         self.__environment = environment
+        self.virtual_coordinate = (0, 0)
 
     def validate_action(self, action):
-        pass
+        return 0 <= virtual_coordinate[0] <= 8 and 0 <= virtual_coordinate[1] <= 8
+
+    def virtual_move(self, action):
+        neighbor = [ \
+            (self.current_coordinate[0] + 1, self.current_coordinate[1]    ), \
+            (self.current_coordinate[0] - 1, self.current_coordinate[1]    ), \
+            (self.current_coordinate[0]    , self.current_coordinate[1] + 1), \
+            (self.current_coordinate[0]    , self.current_coordinate[1] - 1)]
+        self.virtual_coordinate = neighbor[direction]
 
     def move(self, action):
         self._DeterministicPlaceCell__environment.move(action)
+        virtual_coordinate = self._DeterministicPlaceCell__environment.current_coordinate
 
     def coordinate_id(self):
-        coordinate = self._DeterministicPlaceCell__environment.current_coordinate
-        return coordinate[0] + coordinate[1] * self._DeterministicPlaceCell__environment.size[1]
+        return virtual_coordinate[0] + virtual_coordinate[1] * self._DeterministicPlaceCell__environment.size[1]
 
     def novelty(self):
         return self._DeterministicPlaceCell__environment.novelty
