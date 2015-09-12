e8ccaf61cf57968349dfdaf195f2442dddd3f1c7
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-09-13 00:35:32 +0900

    debug __init__.py

--- place_cell/__init__.py
+++ place_cell/__init__.py
@@ -1,3 +1,3 @@
-from agent                    import Agent
+from place_cell               import PlaceCell
 from deterministic_place_cell import DeterministicPlaceCell
 from neural_place_cell        import NeuralPlaceCell
