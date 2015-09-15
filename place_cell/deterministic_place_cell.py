0b83dcc29aae5537e640fb7e9019cb59910d027e
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-09-16 00:06:07 +0900

    modify DeterministicPlaceCell to hold virtual_coordinate and novelty internally

--- place_cell/deterministic_place_cell.py
+++ place_cell/deterministic_place_cell.py
@@ -1,27 +1,34 @@
 from place_cell import PlaceCell
 
 class DeterministicPlaceCell(PlaceCell):
-    def __init__(self, environment):
-        self.__environment = environment
+    def __init__(self):
         self.virtual_coordinate = (0, 0)
+        self.history = {}
+        self.novelty = 0
 
     def validate_action(self, action):
         return 0 <= self.virtual_coordinate[0] <= 8 and 0 <= self.virtual_coordinate[1] <= 8
 
-    def virtual_move(self, action):
+    def move(self, action):
         neighbor = [ \
             (self.virtual_coordinate[0] + 1, self.virtual_coordinate[1]    ), \
             (self.virtual_coordinate[0] - 1, self.virtual_coordinate[1]    ), \
             (self.virtual_coordinate[0]    , self.virtual_coordinate[1] + 1), \
             (self.virtual_coordinate[0]    , self.virtual_coordinate[1] - 1)]
         self.virtual_coordinate = neighbor[action]
-
-    def move(self, action):
-        self._DeterministicPlaceCell__environment.move(action)
-        self.virtual_coordinate = self._DeterministicPlaceCell__environment.current_coordinate
+        self._DeterministicPlaceCell__check_novelty()
 
     def coordinate_id(self):
         return self.virtual_coordinate[0] + self.virtual_coordinate[1] * self._DeterministicPlaceCell__environment.size[1]
 
-    def novelty(self):
-        return self._DeterministicPlaceCell__environment.novelty
+    def __check_novelty(self):
+        flag = False
+        for coordinate in self.history.keys():
+            if coordinate == self.coordinate_id():
+                flag = True
+                break
+        if flag:
+            self.novelty = 0
+        else:
+            self.novelty = 10
+            self.history.append(self.coordinate_id())
