17090618a8fd044c51dc21dbec681f147c7abd9a
ukky17 <i.love.ny517@gmail.com>
2015-09-19 00:12:23 +0900

    executing files

--- train_practice.py
+++ train_practice.py
@@ -11,6 +11,7 @@ import math
 import sys
 import time
 import random
+import pickle
 
 import numpy as np
 import six
@@ -21,11 +22,11 @@ import chainer.functions as F
 from chainer import optimizers
 
 # set parameters
-n_epoch = 500000 # number of epochs
-n_units = 25 # number of units per layer, len(train)=5->20
+n_epoch = 1000000 # number of epochs
+n_units = 25 # number of units per layer, len(train)=5 -> 20 might be the best
 batchsize = 1 # minibatch size
 bprop_len = 1 # length of truncated BPTT
-print_len = n_epoch // 100 # print results
+valid_len = n_epoch // 100 # epoch on which accuracy and perp are calculated
 grad_clip = 5 # gradient norm threshold to clip
 maze_size_x = 9
 maze_size_y = 9
@@ -43,7 +44,7 @@ input data: direction (2D)
 output target: coordinate (converted to 1D)
 """
 def generate_seq(seq_length, maze_size_x, maze_size_y):
-    directions_2d = [] # [x, y], 0.5: left/bottom move, 1: right/top move
+    directions = []
     locations_1d = [] # 1D coorinate
 
     current = (0, 0) # 2D coordinate
@@ -68,12 +69,12 @@ def generate_seq(seq_length, maze_size_x, maze_size_y):
         elif direction == [0.5, 1]:
             current = (current[0]    , current[1] + 1)
         
-        directions_2d.append(direction)
+        directions.append(direction)
         locations_1d.append(current[0]+current[1]*maze_size_x)
         
-    directions_2d = np.array(directions_2d, dtype='float32')
+    directions = np.array(directions, dtype='float32')
     locations_1d = np.array(locations_1d, dtype='int32')
-    return directions_2d, locations_1d
+    return directions, locations_1d
 
 # validation dataset
 valid_data, valid_targets = generate_seq(20, maze_size_x, maze_size_y)
@@ -83,9 +84,9 @@ test_data, test_targets = generate_seq(100, maze_size_x, maze_size_y)
 
 # model
 model = chainer.FunctionSet(
-    x_to_h = F.Linear(2, n_units*4),
-    h_to_h = F.Linear( n_units, n_units*4),
-    h_to_y = F.Linear( n_units, 81))
+    x_to_h = F.Linear(2, n_units * 4),
+    h_to_h = F.Linear(n_units, n_units * 4),
+    h_to_y = F.Linear(n_units, maze_size_x * maze_size_y))
 if args.gpu >= 0:
     cuda.init(args.gpu)
     model.to_gpu()
@@ -126,10 +127,11 @@ def evaluate(data, targets, test=False):
         if test == True:
             print('{} Target: ({}, {})'.format(accuracy.data, t_batch[0] % maze_size_x, 
                 t_batch[0] // maze_size_x))
+            # print('c: {}, h: {}'.format(state['c'].data, state['h'].data)) # show the hidden states
     return int(cuda.to_cpu(sum_accuracy))
                 
 # learning loop iterations
-train_data_length = [5, 10, 20]
+train_data_length = [16]
 for loop in range(len(train_data_length)):
 
     # loop initialization
@@ -140,7 +142,7 @@ for loop in range(len(train_data_length)):
     cur_at = start_at
     epoch = 0
     accum_loss = chainer.Variable(mod.zeros((), dtype=np.float32))
-    print('going to train {} iterations'.format(jump * n_epoch))
+    print('[training] going to train {} iterations'.format(jump * n_epoch))
     
     # loop starts
     while epoch <= n_epoch:
@@ -171,20 +173,20 @@ for loop in range(len(train_data_length)):
             
             sys.stdout.flush()
         
-        if (epoch + 1) % print_len == 0: 
+        if (epoch + 1) % valid_len == 0: 
             
-            # print accuracy, cumulative loss & throuput
+            # calculate accuracy, cumulative loss & throuput
             train_perp = evaluate(train_data, train_targets)
             valid_perp = evaluate(valid_data, valid_targets)
-            perp = math.exp(cuda.to_cpu(cur_log_perp) / print_len)
+            perp = cuda.to_cpu(cur_log_perp) / valid_len
             now = time.time()
-            throuput = print_len / (now - cur_at)
+            throuput = valid_len / (now - cur_at)
             print('epoch {}: train perp: {:.2f} train classified {}/{}, valid classified {}/{} ({:.2f} epochs/sec)'
                 .format(epoch+1, perp, train_perp, whole_len, valid_perp, valid_data.shape[0], throuput))            
             cur_at = now
             
             #  termination criteria
-            if perp < 1.01:
+            if perp < 0.001:
                 break
             else:
                 cur_log_perp.fill(0)   
@@ -192,7 +194,12 @@ for loop in range(len(train_data_length)):
         epoch += 1      
                     
     # Evaluate on test dataset
-    print('test')
+    print('[test]')
     test_perp = evaluate(test_data, test_targets, test=True)
-    print('test classified:', test_perp)
-    
+    print('test classified: {}/{}'.format(test_perp, test_data.shape[0]))
+
+# save the model    
+f = open('pretrained_model_'+str(maze_size_x)+'_'+str(maze_size_y)+'.pkl', 'wb')
+pickle.dump(model, f, 2)
+f.close()  
+                
