696cb88165c138e18c7c842c4dcd1039164a04ff
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-09-22 11:57:09 +0900

    move __check_novelty to parent class, ignore step when move

--- place_cell/deterministic_place_cell.py
+++ place_cell/deterministic_place_cell.py
@@ -1,12 +1,7 @@
 from place_cell import PlaceCell
+import numpy as np
 
 class DeterministicPlaceCell(PlaceCell):
-    def __init__(self, size):
-        self.environment_size = size
-        self.virtual_coordinate = (0, 0)
-        self.history = {}
-        self.novelty = 0
-
     def neighbor(self, action):
         neighbors = [ \
             (self.virtual_coordinate[0] + 1, self.virtual_coordinate[1]    ), \
@@ -20,10 +15,11 @@ class DeterministicPlaceCell(PlaceCell):
         return 0 <= coordinate[0] < self.environment_size[0] and \
                0 <= coordinate[1] < self.environment_size[1]
 
-    def move(self, action, step):
+    def move(self, action):
         self.virtual_coordinate = self.neighbor(action)
-        self._DeterministicPlaceCell__check_novelty()
-        return self._DeterministicPlaceCell__check_steps(step)
+        output = np.zeros(self.environment_size[0] * self.environment_size[1], dtype=np.bool)
+        output[self.coordinate_id()] = 1
+        self._PlaceCell__check_novelty(output)
 
     def coordinate_id(self):
         return self.virtual_coordinate[0] + self.virtual_coordinate[1] * self.environment_size[0]
@@ -32,21 +28,3 @@ class DeterministicPlaceCell(PlaceCell):
         new_x = coordinate_id % self.environment_size[0]
         new_y = (coordinate_id - new_x) / self.environment_size[0]
         self.virtual_coordinate = (new_x, new_y)
-
-    def __check_novelty(self):
-        flag = False
-        for coordinate in self.history.keys():
-            if coordinate == self.coordinate_id():
-                flag = True
-                break
-        if flag:
-            self.novelty = 0
-        else:
-            self.novelty = 10
-
-    def __check_steps(self, step):
-        if self.history.setdefault(self.coordinate_id(), step) <= step:
-            self.history[self.coordinate_id()] = step
-            return True
-        else:
-            return False
--- place_cell/place_cell.py
+++ place_cell/place_cell.py
@@ -1,7 +1,18 @@
 from abc import ABCMeta, abstractmethod
+import numpy as np
 
 class PlaceCell(object):
     __metaclass__ = ABCMeta
+
+    def __init__(self, size):
+        self.environment_size = size
+        self.virtual_coordinate = (0, 0)
+        self.novelty = 0
+        self.novelty_filter = np.zeros(size[0] * size[1], dtype=np.bool)
+
+    def __check_novelty(self, output):
+        self.novelty = 10 if (output & np.logical_not(self.novelty_filter)).any() else 0
+        self.novelty_filter &= output
 
     @abstractmethod
     def validate_action(self, action):
