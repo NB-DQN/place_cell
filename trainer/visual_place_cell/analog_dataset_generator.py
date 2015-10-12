9a2e613dfab10541f659f76e2037e7bff249e56a
ayakato <mitokon0468@gmail.com>
2015-10-12 18:56:18 +0900

    fixed movement

--- trainer/visual_place_cell/analog_dataset_generator.py
+++ trainer/visual_place_cell/analog_dataset_generator.py
@@ -59,32 +59,46 @@ class DatasetGenerator:
         for i in range(0, seq_length):
 
             vel_choice = [x*vel_option for x in range(0, 5)]
+
             
-	          # limit movement
-	    if self.current_coordinate[0] >= self.size[0] - 2:
-                ang_choice.remove = [x for x in range(90, 271, ang_option)] 
-            if self.current_coordinate[0] < 1:
-                ang_choice.remove = [x for x in range(0, 91, ang_option)] + [x for x in range(270, 360, ang_option)]
-            if self.current_coordinate[1] >= self.size[1] - 2:
-                ang_choice = [x for x in range(90, 271, ang_option)]
-            if self.current_coordinate[1] < 1:
+	    # limit movement
+            if self.current_coordinate[0] < 1 and self.current_coordinate[1] < 1:
+                ang_choice = [x for x in range(0, 91, ang_option)]
+            elif self.current_coordinate[0] >= self.size[0] - 2 and self.current_coordinate[1] < 1: 
+                ang_choice = [x for x in range(120, 181, ang_option)]            
+	    elif self.current_coordinate[0] >= self.size[0] - 2 and self.current_coordinate[1] >= self.size[1] - 2:
+                ang_choice = [x for x in range(180, 270, ang_option)] 
+            elif self.current_coordinate[0] < 1 and self.current_coordinate[1] >= self.size[1] - 2:
+                ang_choice = [x for x in range(240, 361, ang_option)]
+                
+	    elif self.current_coordinate[0] >= self.size[0] - 2:
+                ang_choice = [x for x in range(120, 270, ang_option)] 
+            elif self.current_coordinate[0] < 1:
+                ang_choice = [x for x in range(0, 91, ang_option)] + [x for x in range(240, 360, ang_option)]
+            elif self.current_coordinate[1] >= self.size[1] - 2:
                 ang_choice = [x for x in range(180, 361, ang_option)]
+            elif self.current_coordinate[1] < 1:
+                ang_choice = [x for x in range(0, 181, ang_option)]
             else:
 		ang_choice = [x for x in range(0, 360, ang_option) ]
 	
 	    ang = random.choice(ang_choice) 
 	    vel = random.choice(vel_choice)
-            
-	    # move
-	    self.current_coordinate = (self.current_coordinate[0] + vel * math.cos(ang/180*math.pi),
-				       self.current_coordinate[1] + vel * math.sin(ang/180*math.pi))
-            print(self.current_coordinate)
+            print(vel)
+            print(ang)
+            print(math.cos(float(ang)/180*math.pi))
 
-      	    direction = [0] * (360/ang_option)
-	    direction[ang/ang_option] = 1
-            velocity  = [0] * 5
-            velocity[int(vel/vel_option)]  = 1
+	    # move
+            print(self.current_coordinate)
+	    self.current_coordinate = (self.current_coordinate[0] + vel * math.cos(float(ang)/180*math.pi),
+				       self.current_coordinate[1] + vel * math.sin(float(ang)/180*math.pi))
+            print(self.current_coordinate)
             
+            direction = [0] * (360/ang_option+1)
+	    direction[int(ang/ang_option)] = 1
+            velocity  = [0] * 5
+            velocity[int(vel/vel_option)]  = 1        
+
             directions.append(direction)
 	    velocitys.append(velocity)
             image.append(self.visual_image())
