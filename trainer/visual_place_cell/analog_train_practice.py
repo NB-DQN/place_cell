da43f61c4d170dc496880a723e18a5cce1db8775
ukky17 <i.love.ny517@gmail.com>
2015-10-26 04:04:43 +0900

    apply changes in train_practice.py (plot, evaluate etc)

--- trainer/visual_place_cell/analog_train_practice.py
+++ trainer/visual_place_cell/analog_train_practice.py
@@ -15,6 +15,7 @@ import pickle
 
 import numpy as np
 import six
+import matplotlib.pyplot as plt
 
 import chainer
 from chainer import cuda
@@ -32,10 +33,11 @@ valid_len = n_epoch // 100 # 1000 # epoch on which accuracy and perp are calcula
 grad_clip = 5 # gradient norm threshold to clip
 maze_size = (9, 9)
 
+whole_len = 20
+valid_iter = 20
+
 #vel_option = 1
 #ang_option = 90
-
-train_data_length = [20, 100]
 
 # GPU
 parser = argparse.ArgumentParser()
@@ -48,7 +50,10 @@ mod = cuda.cupy if args.gpu >= 0 else np
 dg = DatasetGenerator(maze_size)
 
 # validation dataset
-valid_data = dg.generate_seq(100)
+valid_data_stack = []
+for i in range(valid_iter):
+    valid_data = dg.generate_seq(100)
+    valid_data_stack.append(valid_data)
 
 # test dataset
 test_data = dg.generate_seq(100)
@@ -80,9 +85,9 @@ def forward_one_step(x, t, state, train=True):
     state = {'c': c, 'h': h}
     
     sigmoid_y = 1 / (1 + np.exp(-y.data))
-    square_sum_error = ((t.data - sigmoid_y) ** 2).sum()
+    mean_squared_error = ((t.data - sigmoid_y) ** 2).sum() / t.data.size
 
-    return state, F.sigmoid_cross_entropy(y, t), square_sum_error
+    return state, F.sigmoid_cross_entropy(y, t), mean_squared_error
 
 # initialize hidden state
 def make_initial_state(batchsize=batchsize, train=True):
@@ -99,80 +104,117 @@ def evaluate(data, test=False):
     for i in six.moves.range(len(data['input'])):
         x_batch = mod.asarray([data['input'][i]], dtype = 'float32')
         t_batch = mod.asarray([data['output'][i]], dtype = 'int32')
-        state, loss, square_sum_error = forward_one_step(x_batch, t_batch, state, train=False)
-        sum_error += square_sum_error
-    return sum_error
+        state, loss, mean_squared_error = forward_one_step(x_batch, t_batch, state, train=False)
+        sum_error += mean_squared_error
+    return sum_error / len(data['input'])
 
-# learning loop iterations
-for loop in range(len(train_data_length)):
+# loop initialization
+jump = whole_len // batchsize # = whole len
+cur_log_perp = mod.zeros(())
+start_at = time.time()
+cur_at = start_at
+epoch = 0
+accum_loss = chainer.Variable(mod.zeros((), dtype=np.float32))
+print('[train]')
+print('going to train {} epochs'.format(n_epoch))
 
-    # loop initialization
-    whole_len = train_data_length[loop]
-    jump = whole_len // batchsize # = whole len
-    cur_log_perp = mod.zeros(())
-    start_at = time.time()
-    cur_at = start_at
-    epoch = 0
-    accum_loss = chainer.Variable(mod.zeros((), dtype=np.float32))
-    print('[train]')
-    print('going to train {} iterations'.format(jump * n_epoch))
+# stack errors
+train_errors = np.zeros(n_epoch / valid_len + 1)
+valid_errors_mean = np.zeros(n_epoch / valid_len + 1)
+valid_errors_se = np.zeros(n_epoch / valid_len + 1)
 
-    # loop starts
-    while epoch <= n_epoch:
+# loop starts
+while epoch <= n_epoch:
 
-        # initialize hidden state to 0
-        state = make_initial_state()
+    # initialize hidden state to 0
+    state = make_initial_state()
 
-        # train dataset
-        train_data = dg.generate_seq(whole_len)
+    # train dataset
+    train_data = dg.generate_seq(whole_len)
 
-        for i in six.moves.range(jump):
+    if epoch % valid_len == 0:
 
-            # forward propagation
-            x_batch = mod.array([train_data['input'][i]],  dtype = 'float32')
-            t_batch = mod.array([train_data['output'][i]], dtype = 'int32')
-
-            state, loss_i, acc_i = forward_one_step(x_batch, t_batch, state)
-            accum_loss += loss_i
-            cur_log_perp += loss_i.data.reshape(())
-
-            # truncated BPTT
-            if (i + 1) % bprop_len == 0:
-                optimizer.zero_grads()
-                accum_loss.backward()
-                accum_loss.unchain_backward()  # truncate
-                accum_loss = chainer.Variable(mod.zeros((), dtype=np.float32))
-                optimizer.clip_grads(grad_clip) # gradient clip
-                optimizer.update()
-
-            sys.stdout.flush()
-
-        if (epoch + 1) % valid_len == 0:
-
-            # calculate accuracy, cumulative loss & throuput
-            train_perp = evaluate(train_data)
-            valid_perp = evaluate(valid_data)
+        # calculate accuracy, cumulative loss & throuput
+        train_perp = evaluate(train_data)
+        valid_perp_stack = np.zeros(valid_iter)
+        for i in range(valid_iter):
+            valid_perp = evaluate(valid_data_stack[i])
+            valid_perp_stack[i] = valid_perp
+        valid_perp_mean = np.mean(valid_perp_stack, axis=0)
+        valid_errors_mean[epoch / valid_len] = valid_perp_mean
+        valid_perp_se = np.std(valid_perp_stack, axis=0) / np.sqrt(valid_iter)
+        valid_errors_se[epoch / valid_len] = valid_perp_se
+        train_errors[epoch / valid_len] = train_perp
+        
+        if epoch == 0:
+            perp = None
+        else:
             perp = cuda.to_cpu(cur_log_perp) / valid_len
-            now = time.time()
+            perp = int(perp * 100) / 100.0
+            
+        now = time.time()
+        
+        if epoch == 0:
+            throuput =  0.0
+        else:
             throuput = valid_len / (now - cur_at)
-            print('epoch {}: train perp: {:.2f} train square-sum error: {:.2f}, valid square-sum error: {:.2f} ({:.2f} epochs/sec)'
-                    .format(epoch+1, perp, train_perp, valid_perp, throuput))
-            cur_at = now
+            
+        print('epoch {}: train perp: {} train mean squared error: {:.5f}, valid mean squared error: {:.5f} ({:.2f} epochs/sec)'
+                .format(epoch, perp, train_perp, valid_perp_mean, throuput))
+                
+        cur_at = now
+        
+        #  termination criteria
+        # if perp < 0.001:
+        #     break
+        # else:
+        #     cur_log_perp.fill(0)
+        cur_log_perp.fill(0)
+    
+    for i in six.moves.range(jump):
 
-            #  termination criteria
-            if perp < 0.001:
-                break
-            else:
-                cur_log_perp.fill(0)
+        # forward propagation
+        x_batch = mod.array([train_data['input'][i]],  dtype = 'float32')
+        t_batch = mod.array([train_data['output'][i]], dtype = 'int32')
 
-        epoch += 1
+        state, loss_i, acc_i = forward_one_step(x_batch, t_batch, state)
+        accum_loss += loss_i
+        cur_log_perp += loss_i.data.reshape(())
 
-        # save the model
-        f = open('1023_analog.pkl', 'wb')
-        pickle.dump(model, f, 2)
-        f.close()
+        # truncated BPTT
+        if (i + 1) % bprop_len == 0:
+            optimizer.zero_grads()
+            accum_loss.backward()
+            accum_loss.unchain_backward()  # truncate
+            accum_loss = chainer.Variable(mod.zeros((), dtype=np.float32))
+            optimizer.clip_grads(grad_clip) # gradient clip
+            optimizer.update()
 
-    # Evaluate on test dataset
-    print('[test]')
-    test_square_sum_error = evaluate(test_data, test=True)
-    print('test square-sum error: {:.2f}'.format(test_square_sum_error))
+        sys.stdout.flush()
+
+    epoch += 1
+
+    # save the model
+    f = open('1023_analog.pkl', 'wb')
+    pickle.dump(model, f, 2)
+    f.close()
+
+# Evaluate on test dataset
+print('[test]')
+test_mean_squared_error = evaluate(test_data, test=True)
+print('test mean squared error: {:.5f}'.format(test_mean_squared_error))
+
+# plot
+x = np.arange(0, n_epoch + 1, valid_len)
+plt. plot(x, train_errors, 'bo-')
+plt.hold(True)
+plt. errorbar(x, valid_errors_mean, yerr = valid_errors_se, fmt='ro-')
+plt.title('LSTM errors')
+plt.xlabel('training epochs')
+plt.ylabel('mean squared error')
+plt.legend(['train', 'test'], loc =1)
+plt.ylim([0, 0.05])
+# plt.savefig("figure1.svg")
+# plt.savefig("figure1.png")
+plt.show()
+
